4b7391a33d8769a32a0cf30df1f2e1b00dce0d93
abhishek_pandey <abhishek@test.com>
2023-09-06 18:18:12 +0530

    project added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function TodoItem({ todo, onDelete }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-item">
+      {todo}
+      <button onClick={() => onDelete(todo)}>Delete</button>
     </div>
   );
+}
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const addTodo = () => {
+    if (newTodo.trim() === '') return;
+    setTodos([...todos, newTodo.trim()]);
+    setNewTodo('');
+  };
+
+  const deleteTodo = (todoToDelete) => {
+    const filteredTodos = todos.filter(todo => todo !== todoToDelete);
+    setTodos(filteredTodos);
+  };
+  
+  return (
+    <div className="App">
+      <h1>Simple Todo App</h1>
+      <div className="todo-input">
+        <textarea
+          rows="2" cols="50"
+          value={newTodo}
+          onChange={e => setNewTodo(e.target.value)}
+          placeholder="Add a new todo...">
+          </textarea>
+        <button onClick={addTodo}>Add</button>
+      </div>
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <TodoItem key={index} todo={todo} onDelete={deleteTodo} />
+        ))}
+      </div>
+    </div>
+  );    
 }
 
 export default App;
